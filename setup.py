c86199d02230269b29610301412c1dff16373e7f
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-08-03 21:47:43 +0800

    RELEASE : Bumped version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*parts):
     return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
 
 setup(name="hitchserve",
-      version="0.4.1",
+      version="0.4.2",
       description="Service orchestration library for the Hitch testing framework.",
       long_description=read('README.rst'),
       classifiers=[
